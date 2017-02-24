7b3b2406c358a7c1807ee915e4e61a95973e2ba8
Michael Myers <michael.morris.myers@gmail.com>
2017-02-24 16:34:48 -0500

    removing this

--- typings/array.d.ts
+++ typings/array.d.ts
@@ -1,11 +1,3 @@
 interface Array<T> {
   findIndex(callback: (value: T, index: number, array: T[]) => boolean): number;
 }
-
-interface TooltipProps {
-  tooltipPosition: "vertical" | "horizontal"
-}
-
-interface TooltippedComponentClass {
-    tooltipPosition?: string;
-}
