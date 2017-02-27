844c58c7b81e200dfe55303e4ec0a6a3bf9f77ae
Michael Myers <michael.morris.myers@gmail.com>
2017-02-27 12:53:27 -0500

    adding react-dates

---
+++ typings/react-dates.d.ts
@@ -0,0 +1,11 @@
+declare module "react-dates" {
+    import * as React from "react";
+    import * as moment from "moment";
+
+    export interface DateRangePickerProps {
+
+    }
+
+    export class DateRangePicker extends React.Component<DateRangePickerProps, any> {}
+
+}
