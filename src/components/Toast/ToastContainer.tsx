f59cb893ec70f96ecf8a295e88b6df45b78a35b4
Rey Cegar Pier Castaneda Haro <rey@bespoken.tools>
2017-11-21 17:20:59 -0500

    fix toast styles + properties

--- src/components/Toast/ToastContainer.tsx
+++ src/components/Toast/ToastContainer.tsx
@@ -5,18 +5,23 @@ import { removeToast } from "../../actions/toast";
 import { State } from "../../reducers";
 import Toast, {ToastProps} from "./Toast";
 
+let toastTheme = require("./Toast-theme.scss");
+
 const Toasts = (props: any) => {
     const { removeToast } = props.actions;
     return (
-        <ul className="toasts">
+        <ul className={toastTheme.toasts}>
             {props.toasts && props.toasts.map((toast: ToastProps) => {
                 const { id, onToastClick, ...others } = toast;
                 const removeToastFunction = () => {
                     onToastClick();
                     removeToast(id);
                 };
+                const closeToastFunction = () => {
+                    removeToast(id);
+                }
                 return (
-                    <Toast {...others} key={id} onToastClick={removeToastFunction} />
+                    <Toast {...others} key={id} onToastClick={removeToastFunction} onCloseToast={closeToastFunction} />
                 );
             })}
         </ul>
